6c4705a6cdea08fbe3c52cfcdb68afa2f5608c39
dennyNDTC <56007132+dennyNDTC@users.noreply.github.com>
2022-07-13 13:16:58 -0400

    escape apostrophe

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -44,7 +44,7 @@ export default function Modal({ isOpen, setIsOpen }) {
                     className="inline-block mb-4 py-8 text-xl text-center w-full rounded-lg text-white bg-gray-700 hover:bg-green-500 focus:ring-4 focus:ring-blue-300 focus:outline-none"
                     onClick={() => setIsOpen(false)}
                   >
-                    Yes, I'm ready to download &amp; install!
+                    Yes, I&apos;m ready to download &amp; install!
                   </a>
                   <Link href="/flashcards/purchase?lang=japanese">
                     <a className="inline-block mb-4 py-8 text-xl text-center w-full rounded-lg text-white bg-gray-700 hover:bg-green-500 focus:ring-4 focus:ring-blue-300 focus:outline-none">
